656137ca5b736d769ae27ee260da379bc48ad251
Javier Herrera <jaaah0010@gmail.com>
2021-11-09 20:07:51 -0600

    Segundo Commit

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ let intervalId;
 let enemies = [];
 const bullets = [];
 let isGameOver = false; 
+let score = 0;
+let lives = 1;
+/* const GRAVITY;  */
 
 
 // 3. Crear clases y sus propiedades y metodos
@@ -64,7 +67,7 @@ class BackgroundBoard extends GameAsset {
 
 // Dentro de esta se genera la clase del personaje (CHARACTER)
 class Impostor extends GameAsset {
-    constructor(x, y, width, height, img){
+    constructor(x, y, width, height, img){ // IMG se removera para hacer los sprites del MONO
         super(x, y, width, height, img);
     }
     draw(){
@@ -73,7 +76,7 @@ class Impostor extends GameAsset {
         if(this.y > $canvas.height - this.height) // Esto genera que el personaje no pase de los bordes del canvas en su altura 
             this.y = $canvas.height - this.height;
         this.move = 24;
-        this.jump = 40;
+        this.jump = 80;
         this.fall = 35;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
     }
@@ -95,21 +98,38 @@ class Impostor extends GameAsset {
     moveRight(){
         this.x+= this.move;
     }
+
+
+    // Metodo para saber si se estan tocando mis obstaculos y mi personaje
+
+    isTouching(obj) {
+		return (
+			this.x < obj.x + obj.width &&
+			this.x + this.width > obj.x &&
+			this.y < obj.y + obj.height &&
+			this.y + this.height > obj.y
+		);
+	}
 }
 
 
-// Dentro de esta se genera el obstaculo (OBSTACULO)
-class Obstacle extends GameAsset {
-    constructor(x, y, width, height, img){
-        super($canvas.width, y, width, height, img)
+
+
+class Obstacle {
+    constructor(x, y, width, height, img) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = new Image();
+        this.image.src = "/images/bullet.png";
     }
-    draw(){
+    
+    draw() {
         this.x--;
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
-    }
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    } 
 }
-
-
 
 
 
@@ -123,26 +143,20 @@ const boardImage = "https://64.media.tumblr.com/37ae369d5ee8576431758ad00e4f2f93
 const board = new BackgroundBoard(0, 0, $canvas.width, $canvas.height, boardImage);
 
 
-//(MULTIPLES OPCIONES DE BACKGROUND)
-// https://64.media.tumblr.com/007fea157194d0ef0ea3c99d92c9f439/tumblr_nfkfq2IK1J1tofcqeo1_1280.jpg
-// https://64.media.tumblr.com/667aae89736c77b8d7420f874f04a9b8/tumblr_o5bmkjS7LN1tfav9mo1_500.gifv
-// https://cdnb.artstation.com/p/assets/images/images/003/733/755/large/mark-kirkpatrick-mk-landscape-04.jpg
-// https://64.media.tumblr.com/37ae369d5ee8576431758ad00e4f2f93/tumblr_obik5y31uA1qe6rsgo1_1280.gifv
 
+// (SPRITES, esto se remueve para crear los sprites de mi mono)
 const actorImage = "/images/character.jpg";
 const actor = new Impostor(120, 450, 55, 55, actorImage);
 
-const enemyImage = "";
-const enemy = new Obstacle();
+
 
 
 
 // 5. Funciones principales 
 
-
-
 function start() {
-    setInterval(() => {
+    if(intervalId) return; // Esto se hace para que no se encimen los intervalos y no modificar su velocidad
+    intervalId = setInterval(() => {
         update();
     }, 1000/60)
 }
@@ -152,29 +166,99 @@ function update() {
     // 1. Calcular y recalcular el estado de nuestro programa
     frames++; // Se actualiza el programa
     checkKeys();
-    generateEnemies();
+    generateObstacles(); // Se generan los obstaculos
+    checkCollitions();  // Se checa si hubo colisiones
+    
+ 
 
     // 2. Limpiar el canvas
     clearCanvas();
 
     // 3. Dibujar los elementos
-    board.draw()
-    actor.draw();
-    enemy.draw();
+    board.draw() // Se dibuja el canvas
+    actor.draw(); // Se dibuja al personaje principal
+    drawEnemies(); // Se dibujan los Obstaculos
+    drawScore();
+    drawLives();
+    gameOver(); 
+
 }
+
+
+function drawScore() { // Funcion que me permite dibujar el score (SCORE)
+    score = Math.floor(frames / 5)
+    ctx.font = "20px Impact"
+    ctx.fillStyle = "orange";
+    ctx.fillText(`Score: ${score}`, 20, 30)
+}
+
+
+
+function drawLives(){ // Funcion que me permite dibujar (VIDAS)
+    ctx.font = "20px Impact"
+    ctx.fillStyle = "orange";
+    ctx.fillText('LIVES:  '+ lives, 20, 50)
+}
+
+
+
+
+function gameOver() { // Funcion que dice Game Over cuando hay una colision (GAMEOVER)
+    if(isGameOver){
+        ctx.font = "100px Impact";
+        ctx.fillStyle = "red"
+        ctx.fillText("GAME OVER", 200, 200, 400,);
+    }
+}
+
+
+
+
 
 
 
 // 6. Funciones de apoyo
 
 
-function clearCanvas() {
+ 
+function checkCollitions() { // Funcion que checa si hubo colision de mi personaje y obstaculos (CHECKCOLITIONS)
+    enemies.forEach(enemie => {
+        if(actor.isTouching(enemie)){
+           clearInterval(intervalId);
+           isGameOver = true;
+           
+        }
+    })
+}
+
+
+
+function generateObstacles() { // En esta funcion se generan los obstaculos de forma aletoria en el canvas y se empujan al array.
+    if(frames % 150 === 0) {
+        const y = Math.floor(Math.random() * 380)
+        const enemie = new Obstacle($canvas.width, y, 45, 45, this.image); 
+        enemies.push(enemie);
+    }
+
+    enemies.forEach((obs, index) => { // Eliminamos los enemies que estan fuera de pantalla, para no saturar memoria
+        if (obs.x + obs.width < 0) enemies.splice(1, index)
+    });
+}
+
+
+function drawEnemies() { // En esta funcion se itera cada elemento de enemies y lo imprime
+    enemies.forEach((enemie) => enemie.draw())
+}
+
+
+
+function clearCanvas() { // En esta funcion se limpia el canvas
     ctx.clearRect(0, 0, $canvas.width, $canvas.height); 
 }
 
 
 
-function checkKeys() {
+function checkKeys() { // Funcion que permite ver que boton usa el usuario
     document.onkeydown = event => {
         switch (event.key) {
             case "ArrowUp":
@@ -198,7 +282,7 @@ function checkKeys() {
     
                 break;
             
-        
+
             default:
                 break;
         }
@@ -208,13 +292,19 @@ function checkKeys() {
 
 
 
-// FALTA GENERAR ENEMIGOS DE FORMA ALEATORIA
-function generateEnemies() {
-    if(frames % 300 === 0){
-        const enemy = new Obstacle(500, 380);
-        enemies.push(enemy);
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
